9bfa3b60dfd57626507e1ca3d52390e62ccf9b87
UlaRzyj <urszula.zyjewska@cyfroweszkoly.pl>
2026-02-25 15:25:47 +0100

    Dodanie zapytania z operatorem find

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -1,1 +1,8 @@
+db = db.getSiblingDB('mongoflix')
 
+print("\n--- RAPORT ANALITYCZNY BAZY MONGOFLIX ---")
+
+// Jakie filmy są z gatunku sci-fi?
+print("\n1. Filmy z gatunku 'sci-fi':")
+const sciFi = db.filmy.find({gatunki: "sci-fi"}, {_id: 0, tytuł: 1}).toArray();
+printjson(sciFi)
